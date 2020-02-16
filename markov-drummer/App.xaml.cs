638cbb522b3bd31d1cca16d11ed266a38fd3daf6
Alexander Lysak <alexander.lysak@gmail.com>
2020-02-16 21:05:42 +0200

    Add project files.

---
+++ markov-drummer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace markov_drummer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
